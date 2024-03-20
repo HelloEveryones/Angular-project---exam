02ef08e176076e3f876e4f03ca840ce8991c6e72
Iva <iva_dkostadinova@abv.bg>
2024-03-20 11:03:01 +0200

    create components : loading spiner and no movies in the shared module

--- movies-world/src/app/shared/shared.module.ts
+++ movies-world/src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { NoMoviesComponent } from './no-movies/no-movies.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoadingSpinnerComponent,
+    NoMoviesComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[LoadingSpinnerComponent,NoMoviesComponent]
 })
 export class SharedModule { }
