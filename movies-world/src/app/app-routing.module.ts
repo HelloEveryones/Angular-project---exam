0c7157691e6a5492c4053d299fea9bb8a6e71dcb
Iva <iva_dkostadinova@abv.bg>
2024-03-21 11:04:57 +0200

    create routing path for errors

--- movies-world/src/app/app-routing.module.ts
+++ movies-world/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './feature/home/home.component';
 import { DashboardComponent } from './feature/dashboard/dashboard.component';
+import { ErrorPageComponent } from './core/error-page/error-page.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'/home', pathMatch: 'full'}
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '404', component: ErrorPageComponent},
+  {path: '**', component: ErrorPageComponent}
 ];
 
 @NgModule({
