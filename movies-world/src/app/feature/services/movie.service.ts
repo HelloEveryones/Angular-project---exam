11db45fdf190838ec6ad293824ccf632b2a4855b
Iva <iva_dkostadinova@abv.bg>
2024-03-22 11:11:32 +0200

    create methods in the movie service

--- movies-world/src/app/feature/services/movie.service.ts
+++ movies-world/src/app/feature/services/movie.service.ts
@@ -13,4 +13,27 @@ export class MovieService {
   getMovies() { 
     return this.http.get<Movie[]>(`${environment.apiUrl}/movies`);
   }
+
+  getMovie(id: string){
+    return this.http.get<Movie[]>(`${environment.apiUrl}/movies/${id}`);
+  }
+
+  createMovie(title: string, director: string, year: number, genre: string, imageUrl: string, plot: string) {
+    return this.http.post<Movie>(`${environment.apiUrl}/movies`, { title, director, year, genre, imageUrl, plot });
+  }
+
+  editMovie(title: string, director: string, year: number, genre: string, imageUrl: string, plot: string, movieId:string) {
+    return this.http.post<Movie>(`${environment.apiUrl}/movies/${movieId}/edit`, { title, director, year, genre, imageUrl, plot });
+  }
+
+  deleteMovie(movieId:string) {
+    return this.http.post<Movie>(`${environment.apiUrl}/movies/${movieId}/delete`, {});
+  }
+
+  getMovieWithLimit(limit?: number) {
+    const limitFilter = limit ? `?limit=${limit}` : '';
+
+    return this.http.get<Movie[]>(`${environment.apiUrl}/movies/latest${limitFilter}`);
+  }
+  
 }
