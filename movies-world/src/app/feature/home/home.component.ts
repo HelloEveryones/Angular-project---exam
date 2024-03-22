528b178c517b81678744ee6d88a251e796d15896
Iva <iva_dkostadinova@abv.bg>
2024-03-22 11:28:37 +0200

    last 5 movies

--- movies-world/src/app/feature/home/home.component.ts
+++ movies-world/src/app/feature/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/Types/Movie';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit{
+  movies: Movie[] = []
+  constructor(private movieService: MovieService) {}
+ngOnInit(): void {
+  this.movieService.getMovieWithLimit(5).subscribe((movies)=>{
+    this.movies=movies;
+  })
+}
 }
