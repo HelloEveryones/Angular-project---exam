c32c9833c92f76c5c300c5726323f1441314d8d8
Iva <iva_dkostadinova@abv.bg>
2024-03-26 15:04:24 +0200

    movie details

--- movies-world/src/app/feature/movie-details/movie-details.component.ts
+++ movies-world/src/app/feature/movie-details/movie-details.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/Types/Movie';
+import { MovieService } from '../services/movie.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.css']
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
+  movie: Movie | undefined;
+  movieId = this.route.snapshot.params["id"];
 
+  constructor(private movieService: MovieService, private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.movieService.getMovie(`/${this.movieId}`).subscribe(movie=>{
+      this.movie = movie;
+
+    })
+  }
+
+  deleteHandler():void{
+
+  }
 }
+
