39706c8c6a4ffd2aff141b47bae40a43c106109f
Iva <iva_dkostadinova@abv.bg>
2024-03-26 13:54:19 +0200

    home and movie create

--- movies-world/src/app/feature/home/home.component.ts
+++ movies-world/src/app/feature/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from 'src/app/Types/Movie';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-home',
@@ -14,22 +13,18 @@ export class HomeComponent implements OnInit {
   noMovies = true;
   isLoading = true;
 
-  constructor(private http: HttpClient) { }
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
-    this.getMovies();
-  }
+    this.movieService.getMovieWithLimit(5).subscribe((movies) => {
+      this.movies = movies;
+      console.log(movies);
 
-  getMovies() {
-    return this.http.get<Movie[]>(environment.apiUrl + "/movies").subscribe(movies => {
-      this.movies = movies.sort((a: Movie, b: Movie) => a.created_at.localeCompare(b.created_at)).slice(5);
       this.isLoading = false;
-      if (!this.movies) {
+
+      if (this.movies) {
         this.noMovies = false;
       }
-      console.log(this.movies)
-    })
+    });
   }
-
-
 }
--- movies-world/src/app/feature/movie-create/movie-create.component.ts
+++ movies-world/src/app/feature/movie-create/movie-create.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MovieService } from '../services/movie.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-create',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./movie-create.component.css']
 })
 export class MovieCreateComponent {
+constructor(private movieService: MovieService, private router: Router) { }
 
+submitHandler(form: NgForm) {
+  if (form.invalid) {
+    return;
+  }
+  const { title, director, year, genre, imageUrl, plot } = form.value;
+
+  this.movieService.createMovie( title, director, year, genre, imageUrl, plot ).subscribe(()=>{
+    this.router.navigate(["dashboard"]);
+  })
+}
 }
