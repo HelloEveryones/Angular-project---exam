a217628fc8471eacc93834022f6ebf1e63c536cb
Iva <iva_dkostadinova@abv.bg>
2024-10-02 10:24:00 +0300

    acccess control

--- server/index.js
+++ server/index.js
@@ -18,6 +18,12 @@ dbConnector()
       origin: config.origin,
       credentials: true
     }));
+    app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+    
 
     app.use('/api', apiRouter);
 
