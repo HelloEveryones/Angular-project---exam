4a1be9cde6ed7b7ede1c0774ff72882aee428b71
Iva <iva_dkostadinova@abv.bg>
2025-02-22 18:52:08 +0200

    Css fixes

--- movies-world/src/app/core/navigation/navigation.component.ts
+++ movies-world/src/app/core/navigation/navigation.component.ts
@@ -3,28 +3,36 @@ import { Router } from '@angular/router';
 import { UserService } from 'src/app/feature/services/user.service';
 
 @Component({
-  selector: 'app-navigation', // Селекторът на компонента, който ще се използва в шаблона HTML
-  templateUrl: './navigation.component.html', // Път до шаблона на компонента
-  styleUrls: ['./navigation.component.css'] // Път до CSS стиловете на компонента
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
-  
-  constructor(private userService: UserService, private router: Router) {} // Конструктор на компонента, който приема UserService и Router
-  
+  isMenuOpen = false; // Дефинираме променлива за бургер менюто
+
+  constructor(private userService: UserService, private router: Router) {}
+
   // Метод за проверка дали потребителят е в момента влезнал в системата
   get isLoggedIn(): boolean {
-    return this.userService.isLoggedIn; // Връща стойността на isLoggedIn от UserService
+    return this.userService.isLoggedIn;
   }
 
   // Метод за излизане на потребителя от системата
   logoutHandler(): void {
-    this.userService.logout().subscribe({ // Извикване на метода logout() от UserService чрез HTTP заявка
-      next: () => { // Ако заявката завърши успешно
-        this.router.navigate(['/login']); // Пренасочване към страницата за вход
+    this.userService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+        this.isMenuOpen = false; // Затваряме бургер менюто при изход
       },
-      error: () => { // Ако възникне грешка при заявката
-        this.router.navigate(['/login']); // Пренасочване към страницата за вход
+      error: () => {
+        this.router.navigate(['/login']);
+        this.isMenuOpen = false;
       },
     });
+  }
+
+  // ✅ Метод за отваряне/затваряне на бургер менюто
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 }
